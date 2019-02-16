05c431b6611baeb8b9ebd02a0d29354ab5b32fbe
leadingtw <leadingtw@gmail.com>
2019-02-16 20:36:23 +0800

    [add] vue cli3 plugin

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,13 +2,21 @@ module.exports = {
     root: true,
     env: {
         node: true,
+        es6: true,
     },
-    extends: ['plugin:vue/essential', '@vue/prettier'],
+    extends: ['plugin:vue/essential', '@vue/prettier', 'eslint:recommended'],
+    plugins: ['vue', 'prettier'],
     rules: {
+        'prettier/prettier': [
+            'off',
+            {
+                htmlWhitespaceSensitivity: 'ignore',
+            },
+        ],
         'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
         'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     },
     parserOptions: {
         parser: 'babel-eslint',
     },
-};
+}
