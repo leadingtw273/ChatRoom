9543d16c6f99148476591526f0b8af09e3d6400d
leadingtw273 <leadingtw@gmail.com>
2019-04-11 06:50:25 +0800

    [deploy] express static +

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require('path');
 const app = express();
 
 app.use(history());
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use('/chatroom', express.static(path.join(__dirname, 'dist')));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
