a2d7d90bfaa9fbf18637c01a66312bc642433e83
leadingtw273 <leadingtw@gmail.com>
2019-04-15 19:46:23 +0800

    [refactor] deploy file.

--- app.js
+++ app.js
@@ -3,16 +3,17 @@ const history = require('connect-history-api-fallback');
 const path = require('path');
 const app = express();
 
-const myLogger = function(req, res, next) {
+const showLogger = function(req, res, next) {
   console.log('LOGGED: ', req.path);
   next();
 };
 
 app.use(history());
-app.use(myLogger);
+app.use(showLogger);
 app.use(express.static(path.join(__dirname, 'dist')));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`app listening on port ${port}.`);
+  console.log(`App listening on port ${port}.`);
+  console.log(`Service run on ${process.env.NODE_ENV}.`);
 });
--- src/router.js
+++ src/router.js
@@ -9,7 +9,7 @@ Vue.use(Router);
 
 export default new Router({
   mode: 'history',
-  base: '/chatroom/',
+  base: process.env.BASE_URL,
   routes: [
     {
       path: '/',
