15c69d702da121d643d4a80b46591800c27515f7
leadingtw273 <leadingtw@gmail.com>
2019-04-15 18:52:24 +0800

    [deploy] change static file path

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const myLogger = function(req, res, next) {
 
 app.use(history());
 app.use(myLogger);
-app.use('/chatroom', express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, 'dist')));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
