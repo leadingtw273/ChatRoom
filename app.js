529094d40871dd8416445e956dac5356c69b5ab1
leadingtw273 <leadingtw@gmail.com>
2019-04-15 17:10:35 +0800

    [develop] try again 1

--- app.js
+++ app.js
@@ -3,8 +3,14 @@ const history = require('connect-history-api-fallback');
 const path = require('path');
 const app = express();
 
+const myLogger = function(req, res, next) {
+  console.log('LOGGED: ', req.path);
+  next();
+};
+
 app.use(history());
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(myLogger);
+app.use('/chatroom', express.static(path.join(__dirname, 'dist')));
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
